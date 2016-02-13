5af449dbba901c90efda1abf5fa0f0a06de67100
phosphene <ed@cronos.net>
2016-02-13 14:59:21 -0500

    add carrierwave specific helper code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,17 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+
+  # spec_helper.rb
+  config.after(:each) do
+    if Rails.env.test? || Rails.env.cucumber?
+      FileUtils.rm_rf(Dir["#{Rails.root}/spec/fixtures/uploads"])
+    end
+  end
+
+
+
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
