fde153f3c19a226919bc9fe1c04c3b4e794f3697
phosphene <ed@cronos.net>
2016-02-13 20:32:40 -0500

    mod helper for cwave testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,7 +42,7 @@ RSpec.configure do |config|
 
   config.after(:all) do
     if Rails.env.test?
-      FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads/myfiles/"])
+      FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads/[^.]*"])
     end
   end
 
