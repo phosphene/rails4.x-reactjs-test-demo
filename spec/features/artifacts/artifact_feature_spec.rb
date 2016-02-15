fd40c10de006587b53cb6ded6f14985122522a4d
phosphene <ed@cronos.net>
2016-02-15 13:08:46 -0800

    passing feature spec for uploads

--- spec/features/artifacts/artifact_feature_spec.rb
+++ spec/features/artifacts/artifact_feature_spec.rb
@@ -8,11 +8,11 @@ feature "Artifact feature" do
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
   end
   scenario 'valid user can upload' do
-    pending
-    visit artifacts_path()
-    attach_file "Artifact", my_artifact.myfile.path
-    click_button "Upload"
-    page.should have_content("success")
+    visit new_artifact_path()
+    ##save_and_open_page
+    attach_file "artifact", my_artifact.myfile.path
+    click_button "Create Artifact"
+   expect(page).to have_content("success")
 
   end
 
