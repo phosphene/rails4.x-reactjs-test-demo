ba66f6b9bd87c1f56b01410654ef593910fbd9e1
phosphene <ed@cronos.net>
2016-02-16 09:09:04 -0800

    mods to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: [:show, :edit, :destroy]
 
   # GET /categories
   # GET /categories.json
@@ -68,6 +68,6 @@ class CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name, :parent_id, :lft, :rgt)
+      params.require(:category).permit(:name, :parent_id)
     end
 end
