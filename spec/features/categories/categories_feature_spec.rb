679df2f3407925c8967e1e3af6b766f9df2ea481
phosphene <ed@cronos.net>
2016-02-16 14:46:26 -0800

    make feature spec read more like a user story

--- spec/features/categories/categories_feature_spec.rb
+++ spec/features/categories/categories_feature_spec.rb
@@ -7,7 +7,7 @@ feature "Category feature" do
     signin(user.email, user.password)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
   end
-  scenario 'valid user can create new category' do
+  scenario 'as a valid user I can create a new category' do
     visit new_category_path()
     fill_in "Name", :with => "Parent Category"
     click_button "Create Category"
@@ -15,7 +15,7 @@ feature "Category feature" do
 
   end
 
-  scenario 'valid user can select valid parent and create a valid child' do
+  scenario 'as a valid user I can select valid parent and create a valid child category' do
     my_parent = create(:category, :parent)
     visit new_category_path()
     fill_in "Name", :with => "A Child"
