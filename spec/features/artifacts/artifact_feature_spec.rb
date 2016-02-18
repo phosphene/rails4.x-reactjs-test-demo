a62820663ba0d95ab6e2bcea6e465d5f929fad03
phosphene <ed@cronos.net>
2016-02-18 15:26:40 -0800

    spec proves unauthorized are denied access

--- spec/features/artifacts/artifact_feature_spec.rb
+++ spec/features/artifacts/artifact_feature_spec.rb
@@ -1,20 +1,25 @@
 feature "Artifact feature" do
-
+  let(:user){create(:user, :archivist)}
+  let(:basic_user){create(:user, email: "test@testme.com")}
   let(:my_artifact){create(:artifact)}
 
-  background do
-    user = create(:user)
+  scenario 'valid artifact user can upload' do
     signin(user.email, user.password)
-    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
-  end
-  scenario 'valid user can upload' do
     visit new_artifact_path()
-    ##save_and_open_page
     attach_file "artifact", my_artifact.myfile.path
     click_button "Create Artifact"
    expect(page).to have_content("success")
-
   end
+
+
+
+  scenario 'basic user cannot access new artifact path' do
+    signin(basic_user.email, basic_user.password)
+    visit new_artifact_path()
+    expect(page).to have_content("Access denied")
+  end
+
+
 
 
 
