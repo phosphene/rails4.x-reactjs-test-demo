f65164da5fddd59cf932d45d78de4927bb81b6f3
phosphene <ed@cronos.net>
2016-02-21 13:09:57 -0800

    db migration for user id assocation to profile model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215030843) do
+ActiveRecord::Schema.define(version: 20160221200533) do
 
   create_table "artifacts", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 20160215030843) do
 
   add_index "comment_hierarchies", ["ancestor_id", "descendant_id", "generations"], name: "comment_anc_desc_udx", unique: true
   add_index "comment_hierarchies", ["descendant_id"], name: "comment_desc_idx"
+
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
